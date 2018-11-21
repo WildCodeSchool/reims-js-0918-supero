1b8d870be7e5c13f653acb74e3620c83863f09ee
Robin Guerre <robin.guerre@yahoo.fr>
2018-11-21 11:03:50 +0100

    made route users .get : se_connecter

--- supero-back/index.js
+++ supero-back/index.js
@@ -1,9 +1,20 @@
 const port = 3000;
 const express = require("express");
 const app = express();
+const json = require("./users.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// USERS -- se connecter
+
+app.get("/api/users", (req, res) => {
+  res.send(
+    `Welcome, you have logged in as : ${
+      json.users[0].pseudo
+    } and you are level ${json.users[0].level}.`
+  );
 });
 
 app.listen(port, err => {
