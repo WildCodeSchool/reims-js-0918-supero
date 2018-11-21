8505bca42e580b9bd3d82ba86f6637f87a806aae
Quentin Da Silva <quentin_51@live.fr>
2018-11-21 11:26:17 +0100

    route /activities and activities/sport_id

--- supero-back/index.js
+++ supero-back/index.js
@@ -1,10 +1,35 @@
 const port = 3000;
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+// Support JSON-encoded bodies
+app.use(bodyParser.json());
+// Support URL-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+// IMPORT JSON
+const activitiesjson = require("./activities.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// ACTIVITIES
+
+app
+  .get("/activities", (req, res) => {
+    res.send(activitiesjson);
+  })
+  .get("/activities/:sports_id", (req, res) => {
+    const sportId = req.params.sports_id;
+    const result = activitiesjson.activities.filter(
+      activity => activity.sports_id.toString() === sportId
+    );
+    res.send(result);
+  });
 
 app.listen(port, err => {
   if (err) {
