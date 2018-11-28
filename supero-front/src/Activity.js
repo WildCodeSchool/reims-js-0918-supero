d60acf789befb7ea839cc0a3303d97a44a87bc5c
Fabien Raymond <voortexxx@gmail.com>
2018-11-28 14:26:51 +0100

    add some style for activity component

--- supero-front/src/Activity.js
+++ supero-front/src/Activity.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "./Activity.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card, CardTitle, CardBody, CardImg, CardImgOverlay } from "reactstrap";
 
 import {
   faBolt,
@@ -14,21 +16,34 @@ const sports = ["Running", "Vélo", "Football", "Sport Ext.", "Sport Int."];
 console.log(faMapMarkerAlt);
 const Activity = ({ activity }) => {
   return (
-    <div className="activity">
-      <h2> Session {sports[activity.activity_id - 1]}</h2>
-      <span>
-        <FontAwesomeIcon icon="bolt" />
-        <FontAwesomeIcon icon="bolt" />
-        <FontAwesomeIcon icon="bolt" />
-      </span>
-      <div>
-        <FontAwesomeIcon icon="clock" />
-        19/11/2018
-        <FontAwesomeIcon icon="map-marker-alt" />
-        Piscine Talleyrand
-        <FontAwesomeIcon icon="user" />
-        Organisé par Aquaman
-      </div>
+    <div className="activity-container">
+      <Card className="activity" inverse>
+        <CardImg
+          width="100%"
+          src="./images/swimming.jpg"
+          alt="Card image cap"
+        />
+        <CardImgOverlay>
+          <CardTitle>
+            <h2> Session {sports[activity.activity_id - 1]}</h2>
+          </CardTitle>
+          <CardBody>
+            <span class="difficulty">
+              <FontAwesomeIcon icon="bolt" />
+              <FontAwesomeIcon icon="bolt" />
+              <FontAwesomeIcon icon="bolt" />
+            </span>
+          </CardBody>
+          <div className="activity_details">
+            <FontAwesomeIcon className="ml-2 mr-1" icon="clock" />
+            19/11/2018
+            <FontAwesomeIcon className="ml-2 mr-1" icon="map-marker-alt" />
+            Reims
+            <FontAwesomeIcon className="ml-2 mr-1" icon="user" />
+            Par Aquaman
+          </div>
+        </CardImgOverlay>
+      </Card>
     </div>
   );
 };
