9b6627247292d71b314fd40e79b9d66e7ded3d78
Fabien Raymond <voortexxx@gmail.com>
2018-11-28 16:25:43 +0100

    fix Display difficulty icons

--- supero-front/src/Activity.js
+++ supero-front/src/Activity.js
@@ -3,7 +3,7 @@ import "./Activity.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, CardTitle, CardBody, CardImg, CardImgOverlay } from "reactstrap";
-import displayDifficultyIcon from "./displayDifficultyIcon";
+import DisplayDifficultyIcon from "./DisplayDifficultyIcon";
 
 import {
   faBolt,
@@ -13,8 +13,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faBolt, faClock, faMapMarkerAlt, faUser);
-
-displayDifficultyIcon(2);
 
 const sports = ["Running", "Vélo", "Football", "Sport Ext.", "Sport Int."];
 
@@ -35,9 +33,7 @@ const Activity = activity => {
           <CardTitle>Session {sports[activity.sports_id - 1]}</CardTitle>
           <CardBody>
             <span className="difficulty">
-              {displayDifficultyIcon(activity.difficulty).map((icon, index) => (
-                <span key={index}>{icon}</span>
-              ))}
+              <DisplayDifficultyIcon difficulty={activity.difficulty} />
             </span>
           </CardBody>
           <div className="activity_details">
