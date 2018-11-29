5de0892d7eb2f72c8d4b11cf80b295e44b36c653
Quentin Da Silva <quentin_51@live.fr>
2018-11-29 11:36:27 +0100

    delete const activities

--- supero-front/src/App.js
+++ supero-front/src/App.js
@@ -3,26 +3,6 @@ import "./App.css";
 import axios from "axios";
 import ActivitiesList from "./ActivitiesList";
 
-const activities = [
-  {
-    activity_id: 1,
-    sports_id: 1,
-    creator_id: 1,
-    difficulty: 3,
-    activity_description: "Yolo",
-    adresse: "31 rue de lorem ipsum",
-    city: "lorem ipsum",
-    latitude: "35.123.78",
-    longitude: "654151651",
-    start_time: "05/12/18",
-    duration: 2,
-    photo: "image.png",
-    max_participants: 5,
-    creation_time: "01/12/18",
-    activity_img: "running.jpg"
-  }
-];
-
 class App extends Component {
   constructor(props) {
     super(props);
