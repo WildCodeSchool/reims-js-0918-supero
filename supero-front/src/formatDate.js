d4daaa44c06c780ffe856f5c20d992b7f4baeda2
Fabien Raymond <voortexxx@gmail.com>
2018-11-29 17:46:07 +0100

    refactor fonction formatDate

--- supero-front/src/formatDate.js
+++ supero-front/src/formatDate.js
@@ -1,9 +1,7 @@
 const { DateTime } = require("luxon");
 
-const formatDate = newDate => {
-  const dateFormated = DateTime.fromISO(newDate)
+const formatDate = newDate =>
+  DateTime.fromISO(newDate)
     .setLocale("fr")
     .toFormat("D");
-  return dateFormated;
-};
 export default formatDate;
