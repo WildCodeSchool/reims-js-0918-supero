c4a4327abe177271475474758c2e3adce3bd0e42
Robin Guerre <robin.guerre@yahoo.fr>
2018-12-04 10:54:48 +0100

    wip form with several pages

--- supero-front/src/App.js
+++ supero-front/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import ActivitiesList from "./ActivitiesList";
 import ActivityDetail from "./ActivityDetail";
 import ActivitiesContainer from "./containers/ActivitiesContainer";
-import AddActivityForm from "./AddActivityForm";
+import AddActivityForm from "./form-addActivity/AddActivityForm";
 import Header from "./Header";
 
 class App extends Component {
