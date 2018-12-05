c519faca84f9112e31163415bba7af4b940151e6
Quentin Da Silva <quentin_51@live.fr>
2018-12-05 11:02:36 +0100

    use reactstrap for form SendSport

--- supero-front/src/form-addActivity/SendSport.js
+++ supero-front/src/form-addActivity/SendSport.js
@@ -1,51 +1,62 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import validate from "./validate";
 import renderField from "./renderField";
 
 const SendSport = props => {
   const { handleSubmit } = props;
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <div>
+          <Label>Indiquez le sport pratiqué :</Label>
+        </div>
+        <Input
+          name="sport_id"
+          tag={Field}
+          type="select"
+          component="select"
+          label="Session"
+        >
+          <option value="1">Running</option>
+          <option value="2">Vélo</option>
+          <option value="3">Natation</option>
+          <option value="4">Musculation</option>
+          <option value="5">Autres sports ext.</option>
+          <option value="6">Autres sports int.</option>
+        </Input>
+      </FormGroup>
+      <FormGroup>
+        <Input
+          name="activity_title"
+          type="text"
+          tag={Field}
+          component={renderField}
+          label="Nom de l'activité"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Input
+          name="activity_difficulty"
+          type="select"
+          tag={Field}
+          component="select"
+          label="Difficulté"
+        >
+          <option>Difficulté</option>
+          <option value="1">Facile</option>
+          <option value="2">Intermediaire</option>
+          <option value="3">Difficile</option>
+          <option value="4">Intense</option>
+        </Input>
+      </FormGroup>
       <div>
-        <label>Indiquez le sport pratiqué :</label>
-      </div>
-      <Field name="sport_id" type="select" component="select" label="Session">
-        <option>Session</option>
-        <option value="1">Running</option>
-        <option value="2">Vélo</option>
-        <option value="3">Natation</option>
-        <option value="4">Musculation</option>
-        <option value="5">Autres sports ext.</option>
-        <option value="6">Autres sports int.</option>
-      </Field>
-      <Field
-        name="activity_title"
-        type="text"
-        component={renderField}
-        label="Nom de l'activité"
-      />
-      <div>
-        <label>Difficulté</label>
-      </div>
-      <Field
-        name="activity_difficulty"
-        type="select"
-        component="select"
-        label="Difficulté"
-      >
-        <option>Difficulté</option>
-        <option value="1">Facile</option>
-        <option value="2">Intermediaire</option>
-        <option value="3">Difficile</option>
-        <option value="4">Intense</option>
-      </Field>
-      <div>
-        <button type="submit" className="next">
+        <Button type="submit" className="next">
           Suivant
-        </button>
+        </Button>
       </div>
-    </form>
+    </Form>
   );
 };
 
--- supero-front/src/form-addActivity/renderField.js
+++ supero-front/src/form-addActivity/renderField.js
@@ -1,10 +1,10 @@
 import React from "react";
+import { Input } from "reactstrap";
 
 const renderField = ({ input, label, type, meta: { touched, error } }) => (
   <div>
-    <label>{label}</label>
     <div>
-      <input {...input} placeholder={label} type={type} />
+      <Input {...input} placeholder={label} type={type} />
       {touched && error && <span>{error}</span>}
     </div>
   </div>
