9c911300d3a7961b7d63dd7dd708961b4c09df87
benoit niveau <be.niveau@hotmail.fr>
2018-12-05 11:51:11 +0100

    fix_bugs

--- supero-front/src/form-addActivity/SendPlace.js
+++ supero-front/src/form-addActivity/SendPlace.js
@@ -9,54 +9,58 @@ class SendPlace extends Component {
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
-  
+    this.handleChange = this.handleChange.bind(this);
   }
-   onChange = date => this.setState({ date });
-     render(){
-  return (
-    <form onSubmit={this.props.handleSubmit}>
-      <Field
-        name="activity_adresse"
-        type="text"
-        component={renderField}
-        label="Adresse"
-      />
-      <Field
-        name="activity_city"
-        type="text"
-        component={renderField}
-        label="Ville"
-      />
-      <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange}
-        showTimeSelect
-        timeFormat="HH:mm"
-        timeIntervals={15}
-        dateFormat="MMMM d, yyyy h:mm aa"
-        timeCaption="time"
-      />
-      <Field
-        name="activity_duration"
-        type="text"
-        component={renderField}
-        label="Durée"
-      />
-      <Field
-        name="activity_max_participants"
-        type="text"
-        component={renderField}
-        label="Limite maximum de participants"
-      />
-      <div>
-        <button type="button" className="previous" onClick={this.props.previousPage}>
-          Précédent
-        </button>
-        <button type="submit" className="next">
-          Suivant
-        </button>
-      </div>
-    </form>
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+}     
+  render(){
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <Field
+          name="activity_adresse"
+          type="text"
+          component={renderField}
+          label="Adresse"
+        />
+        <Field
+          name="activity_city"
+          type="text"
+          component={renderField}
+          label="Ville"
+        />
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          dateFormat="MMMM d, yyyy h:mm aa"
+          timeCaption="time"
+        />
+        <Field
+          name="activity_duration"
+          type="text"
+          component={renderField}
+          label="Durée"
+        />
+        <Field
+          name="activity_max_participants"
+          type="text"
+          component={renderField}
+          label="Limite maximum de participants"
+        />
+        <div>
+          <button type="button" className="previous" onClick={this.props.previousPage}>
+            Précédent
+          </button>
+          <button type="submit" className="next">
+            Suivant
+          </button>
+        </div>
+      </form>
   );}
 };
 
