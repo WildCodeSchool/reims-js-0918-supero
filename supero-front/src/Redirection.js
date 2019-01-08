bab16e975cbf9816e96c0a60df716cff214fc970
Robin Guerre <robin.guerre@yahoo.fr>
2019-01-08 15:39:21 +0100

    redirection links ok

--- supero-front/src/Redirection.js
+++ supero-front/src/Redirection.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./Header";
 import { Button } from "reactstrap";
 import { Card, CardTitle, CardBody, CardImg, CardImgOverlay } from "reactstrap";
+import { Link } from "react-router-dom";
 import "./Redirection.css";
 
 class Redirection extends Component {
@@ -11,21 +12,21 @@ class Redirection extends Component {
         <div style={{ paddingBottom: "10px" }}>
           <Header activitiesView={true} title="Activité créée !" />
         </div>
-        <div className="activity-container">
+        <div className="redirection-container">
           <Card className="activity" inverse>
             <CardImg width="100%" src="./images/default.jpg" alt="default" />
             <CardImgOverlay>
-              <CardTitle>
-                F
-                {/* élicitations, votre activité a été correctement créée ! */}
-              </CardTitle>
+              <CardTitle>Félicitations, votre activité a été créée !</CardTitle>
               <CardBody>
-                Sou
-                {/* haitez-vous retourner vers votre activité ou afficher le flux
-                principal ? */}
+                Souhaitez-vous retourner vers votre activité ou afficher le flux
+                principal ?
               </CardBody>
-              <Button>Retour vers l'activité</Button>
-              <Button>Flux</Button>
+              <Button tag={Link} to="/ActivitiesList">
+                Retour vers l'activité
+              </Button>
+              <Button tag={Link} to="/ActivityDetail/1">
+                Flux
+              </Button>
             </CardImgOverlay>
           </Card>
         </div>
