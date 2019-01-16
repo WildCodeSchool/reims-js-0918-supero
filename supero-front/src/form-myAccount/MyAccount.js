671ed24df8967ba88767c482fbdd587257d5182f
Robin Guerre <robin.guerre@yahoo.fr>
2019-01-16 18:03:55 +0100

    redirection & goBack OK

--- supero-front/src/form-myAccount/MyAccount.js
+++ supero-front/src/form-myAccount/MyAccount.js
@@ -1,7 +1,6 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "../Header";
-// import { Button } from "reactstrap";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Field, reduxForm } from "redux-form";
 import "./myAccount.css";
 import {
@@ -18,11 +17,12 @@ import { toastr } from "react-redux-toastr";
 import axios from "axios";
 
 const MyAccount = props => {
-  const { handleSubmit, pristine, reset, submitting } = props;
+  const goBack = () => props.history.goBack();
+  const { handleSubmit, pristine, submitting } = props;
   return (
     <div style={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
       <div style={{ paddingBottom: "10px" }}>
-        <Header title="Succès !" />
+        <Header title="Vos informations" goBack={goBack} />
       </div>
       <div className="myAccount-container">
         <Container fluid>
@@ -46,7 +46,11 @@ const MyAccount = props => {
                     )
                     .then(res => {
                       if (res.data.toastType !== "error") {
-                        toastr.success("Succès", res.data.message);
+                        toastr.success(
+                          "Informations mises à jour !",
+                          res.data.message,
+                          props.history.push("/ActivitiesList")
+                        );
                       } else {
                         toastr.error("Erreur", res.data.message);
                       }
@@ -55,7 +59,9 @@ const MyAccount = props => {
               >
                 <h2 className="text-center">Mon compte</h2>
                 <FormGroup>
+                  <p>Données personnelles :</p>
                   <Field
+                    title="Votre prénom :"
                     className="Form-Input"
                     name="user_firstname"
                     type="text"
@@ -98,6 +104,43 @@ const MyAccount = props => {
                     label="Date de naissance"
                   />
                 </FormGroup>
+                <p>Votre adresse e-mail :</p>
+                <FormGroup>
+                  <Field
+                    className="Form-Input"
+                    name="user_email"
+                    type="text"
+                    component={renderField}
+                    label="E-mail"
+                  />
+                </FormGroup>
+                <p>Infos supplémentaires :</p>
+                <FormGroup>
+                  <Field
+                    name="user_about"
+                    type="textarea"
+                    component={renderField}
+                    label="Plus d'infos sur vous"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    className="Form-Input"
+                    name="user_level"
+                    type="select"
+                    tag={Field}
+                    component="select"
+                    label="Niveau"
+                  >
+                    <option value="" disabled>
+                      Niveau
+                    </option>
+                    <option value="1">Péon</option>
+                    <option value="2">Amateur</option>
+                    <option value="3">Héros</option>
+                    <option value="4">Demi-dieu</option>
+                  </Input>
+                </FormGroup>
                 <div className="d-flex justify-content-between">
                   <Button
                     type="submit"
@@ -105,6 +148,13 @@ const MyAccount = props => {
                     disabled={pristine || submitting}
                   >
                     Valider
+                  </Button>
+                  <Button
+                    className="button"
+                    tag={Link}
+                    to={`/UserProfile/${props.initialValues.user_id}`}
+                  >
+                    Retour
                   </Button>
                 </div>
               </Form>
