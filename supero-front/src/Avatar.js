2e23bcf1ac739e559bfa5fdaa6ab0c3bccd5cdd8
Quentin Da Silva <quentin_51@live.fr>
2019-01-17 14:28:51 +0100

    conflict avatar

--- supero-front/src/Avatar.js
+++ supero-front/src/Avatar.js
@@ -103,7 +103,7 @@ class Avatar extends Component {
                       fontWeight: 300,
                       fontSize: "small"
                     }}
-                    className = "ml-1"
+                    className="ml-1"
                   >
                     Téléchargez une image au format .png, .jpg, .jpeg
                   </p>
@@ -111,33 +111,6 @@ class Avatar extends Component {
               </Col>
             </Row>
           </ComeFromLeft>
-
-          {/* this row is conflicting */}
-          <Row>
-            <Col xs="12" className="d-flex justify-content-center mb-2 ">
-              {!this.state.enable ? (
-                <i
-                  style={{
-                    fontSize: "150px",
-                    color: "rgba(255, 255, 255,0.5)"
-                  }}
-                  className="fas fa-user"
-                />
-              ) : (
-                <img
-                  style={{
-                    objectFit: "cover",
-                    width: "150px",
-                    height: "150px",
-                    borderRadius: "150px"
-                  }}
-                  alt="profile"
-                  src={this.preview}
-                />
-              )}
-            </Col>
-          </Row>
-
           <Row>
             <Col xs="6" className="d-flex justify-content-center">
               <ComeFromTransparent delay={500}>
